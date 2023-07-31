fd09e7ef377a0482b9dce4d076ac80365e10e79c
SketchPiece <lubkin.andrey@gmail.com>
2023-07-30 19:27:02 -0700

    fix local app view

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
-	import { auth } from '$lib/firebase';
+	import { auth, firestore } from '$lib/firebase';
 	import { cn } from '$lib/utils';
 	import { onAuthStateChanged } from 'firebase/auth';
+	import { doc, getDoc } from 'firebase/firestore';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		onAuthStateChanged(auth, (user) => {
-			if (!user) goto('/signin');
+		onAuthStateChanged(auth, async (user) => {
+			if (!user) return goto('/signin');
+			const userInfo = await getDoc<any, any>(doc(firestore, `users/${user.uid}`));
+			if (userInfo.data().type === 'local') return goto('/chats');
 		});
 	});
 </script>
